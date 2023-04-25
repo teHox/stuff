ec0eb134ff423a1ddc997b16a2803404c5121b2b
Your Name <you@example.com>
2023-04-25 17:31:40 +0200

    Finish project

---
+++ src/components/Products/Product.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { addItemToCart } from "../../features/user/userSlice";
+
+import styles from "../../styles/Product.module.css";
+import { ROUTES } from "../../utils/routes";
+
+const SIZES = [4, 4.5, 5];
+
+export default function Product(item) {
+	const { images, title, price, description } = item;
+
+	const dispatch = useDispatch();
+
+	const [currentImage, setCurrentImage] = useState();
+	const [currentSize, setCurrentSize] = useState();
+
+	useEffect(() => {
+		if (!images.length) return;
+
+		setCurrentImage(images[0]);
+	}, []);
+
+	const addToCart = () => {
+		dispatch(addItemToCart(item));
+	};
+
+	return (
+		<section className={styles.product}>
+			<div className={styles.images}>
+				<div
+					className={styles.current}
+					style={{ backgroundImage: `url(${currentImage})` }}
+				/>
+				<div className={styles["images-list"]}>
+					{images.map((image, i) => (
+						<div
+							key={i}
+							className={styles.image}
+							style={{ backgroundImage: `url(${image})` }}
+							onClick={() => {
+								setCurrentImage(image);
+							}}
+						/>
+					))}
+				</div>
+			</div>
+			<div className={styles.info}>
+				<h1 className={styles.title}>{title}</h1>
+				<div className={styles.price}>{price}$</div>
+				<div className={styles.color}>
+					<span>Color:</span> Green
+				</div>
+				<div className={styles.sizes}>
+					<span>Sizes:</span>
+					<div className={styles.list}>
+						{SIZES.map((size) => (
+							<div
+								key={size}
+								onClick={() => {
+									setCurrentSize(size);
+								}}
+								className={`${styles.size} ${
+									currentSize === size ? styles.active : ""
+								}`}
+							>
+								{size}
+							</div>
+						))}
+					</div>
+				</div>
+				<p className={styles.description}>{description}</p>
+
+				<div className={styles.actions}>
+					<button
+						onClick={addToCart}
+						className={styles.add}
+						disabled={!currentSize}
+					>
+						Add to cart
+					</button>
+					<button className={styles.favourite}>Add to favourites</button>
+				</div>
+
+				<div className={styles.bottom}>
+					<div className={styles.purchase}>
+						{Math.floor(Math.random() * 20 + 1)} people purchased
+					</div>
+					<Link to={ROUTES.HOME}>Return to store</Link>
+				</div>
+			</div>
+		</section>
+	);
+}
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+
+import styles from "../../styles/Sidebar.module.css";
+
+export default function Sidebar() {
+	const { list } = useSelector(({ categories }) => categories);
+
+	return (
+		<section className={styles.sidebar}>
+			<div className={styles.title}>CATEGORIES</div>
+			<nav>
+				<ul className={styles.menu}>
+					{list.map(({ id, name }) => (
+						<li key={id}>
+							<NavLink
+								className={({ isActive }) =>
+									`${styles.link} ${isActive ? styles.active : ""}`
+								}
+								to={`/categories/${id}`}
+							>
+								{name}
+							</NavLink>
+						</li>
+					))}
+				</ul>
+			</nav>
+			<div className={styles.footer}>
+				<a href="/help" target="_blank" className={styles.link}>
+					Help
+				</a>
+				<a
+					href="/terms"
+					target="_blank"
+					className={styles.link}
+					style={{ textDecoration: "underline" }}
+				>
+					Terms & Conditions
+				</a>
+			</div>
+		</section>
+	);
+}
